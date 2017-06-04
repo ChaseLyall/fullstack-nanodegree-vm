0202935e5ad0790fa7c6f4744500ab7d86cb7ebe
Chase <ChaseLyall@users.noreply.github.com>
2017-06-04 16:36:07 -0700

    Udated for Item Catalog Project
    Modified Udacity code for Chase's Catalog Project.

---
+++ vagrant/catalog/README.md
@@ -0,0 +1,51 @@
+# Udacity Full Stack Foundations and Authentication and Authorization
+This is a simple catalog web application built mostly in Python and HTML. It utilizes a Flask and a SQLite backend, plus Google and Facebook sign-in for user authentication.
+
+
+## Getting Started
+Please make sure you have [Virtual Box](https://www.virtualbox.org/wiki/Downloads) and [Vagrant](https://www.vagrantup.com/downloads.html) installed. Then run the following commands from the main folder:
+
+vagrant up
+vagrant ssh
+cd /vagrant/catalog
+python database_setup.py
+python sample_data.py
+python project.py
+http://localhost:8000/
+
+
+## Usage
+The purpose of this project is to illustrate how to build a simple catalog web application that uses a SQLite backend and Google sign-in authentication.
+
+
+## Known Bugs
+No known bugs at this time.
+
+
+## Contributing
+- Chase Lyall
+- based on Udacity's [Full Stack Foundations](https://classroom.udacity.com/courses/ud088) and [Authentication and Authorization](https://classroom.udacity.com/courses/ud330) courses.
+
+
+## License
+MIT License
+
+Copyright (c) 2016 Chase Lyall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,58 +1,37 @@
 #!/usr/bin/env python
-#
+# 
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-import bleach
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
+
 def deleteMatches():
     """Remove all the match records from the database."""
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("DELETE FROM matches")
-    DB.commit()
-    DB.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("DELETE FROM players")
-    DB.commit()
-    DB.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("SELECT COUNT(*) FROM players")
-    counts = cursor.fetchone()
-    count = counts[0]
-    DB.close()
-    return count
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-
+  
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-
+  
     Args:
       name: the player's full name (need not be unique).
     """
-    DB = connect()
-    cursor = DB.cursor()
-    clean_name = bleach.clean(name)
-    cursor.execute("INSERT INTO players (name) VALUES (%s)", (clean_name,))
-    DB.commit()
-    DB.close()
 
 
 def playerStandings():
@@ -68,14 +47,6 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("SELECT id, name, wins, matches FROM records ORDER BY wins DESC")
-    players = []
-    for row in cursor.fetchall():
-        players.append((int(row[0]), str(row[1]), int(row[2]), int(row[3])))
-    DB.close()
-    return players
 
 
 def reportMatch(winner, loser):
@@ -85,22 +56,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("INSERT INTO matches (winner_id, loser_id) VALUES (%s, %s)", (winner, loser,))
-    DB.commit()
-    DB.close()
-
-
-
+ 
+ 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-
+  
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-
+  
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -108,17 +73,5 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    DB = connect()
-    cursor = DB.cursor()
-    cursor.execute("SELECT r.id, p.name FROM records r JOIN players p ON p.id = r.id ORDER BY wins DESC")
-    players = []
-    for row in cursor.fetchall():
-        players.append((int(row[0]), str(row[1])))
 
-    num_players = len(players)
-    matches = []
-    for x in range(0, num_players, 2):
-        matches.append(players[x] + players[x+1])
-    DB.close()
-    return matches
 
